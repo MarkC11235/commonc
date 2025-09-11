654eddfe65d9155e694da3741d209ad98719ba83
Mark Castle <marklouiscastle@gmail.com>
2025-09-11 01:13:37 -0500

    added a cache example, this code can now be generalized into a decorator

--- test.c
+++ test.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct timespec start, end; \
+double time_taken_sec;      \
+double time_taken_nsec;     
+#define TIME_START clock_gettime(CLOCK_MONOTONIC, &start); 
+#define TIME_END                                           \
+   clock_gettime(CLOCK_MONOTONIC, &end);                  \
+   time_taken_sec = (end.tv_sec - start.tv_sec);          \
+   time_taken_nsec = (end.tv_nsec - start.tv_nsec) / 1e9; \
+   printf("elapsed time: %f seconds\n", time_taken_sec + time_taken_nsec);
+#define field(type, name, def) type name;
+#define default_arg(type, name, def) .name = def,
+#define unpack(type, name, def) type name = ops.name;
+#include <stdio.h>
 #include "common.h"
 
 def_list(int);
@@ -28,8 +44,8 @@ int sum_nocache(int x){
     return sum;
 }
 
-@time
 void test_sum(int n, int runs, bool cache){
+    TIME_START
     if(cache){
         for(int i = 0; i < runs; i++) {
             int s = sum(n);
@@ -42,6 +58,7 @@ void test_sum(int n, int runs, bool cache){
             //printf("sum %d: %d\n", n, s);
         }
     }
+    TIME_END
 }
 
 
